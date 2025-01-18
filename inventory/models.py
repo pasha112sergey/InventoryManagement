a42be64a7b885f23f2bce4e39d981f88cced5228
Sergey Safronov <sergeysafronov@Sergeys-Air.lan>
2025-01-18 14:54:43 -0500

    Fix migration issues for Inventory model

--- inventory/models.py
+++ inventory/models.py
@@ -2,15 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
 class Inventory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length = 100, null=False, blank=False)
-    cost_per_item = models.DecimalField(max_digits=19, decimal_places=2, null=False, blank=False)
-    quantity_in_stock = models.IntegerField(null=False, blank=False)
-    quantity_sold = models.IntegerField(null=False, blank=False)
-    sales = models.DecimalField(max_digits=19, decimal_places=2, null=False, blank=False)
-    stock_date = models.DateField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    cost_per_item = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity_in_stock = models.PositiveIntegerField()
+    quantity_sold = models.PositiveIntegerField()
+    sales = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_date = models.DateField(auto_now=True)
     last_sales_date = models.DateField(auto_now=True)
+
+
+    def __str__ (self) -> str:
+        return f"{self.name} ({self.user.username})"
+
 
     def __str__ (self) -> str:
         return f"{self.name} ({self.user.username})"
