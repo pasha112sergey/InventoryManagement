a6947f3b2ddbe24da581906c6c5bb21e4b10a2e8
Sergey Safronov <sergeysafronov@Sergeys-Air.lan>
2025-01-18 14:59:41 -0500

    Set DEBUG to False for production

--- inventorySystem/settings.py
+++ inventorySystem/settings.py
@@ -88,17 +88,11 @@ WSGI_APPLICATION = 'inventorySystem.wsgi.application'
 #     }
 # }
 
-if os.getenv("DATABASE_URL"):
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
+
 
 
 
